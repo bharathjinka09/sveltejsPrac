5ef6be824be1bfd123226b2d51e9deac6b7b9a58
bharathjinka09 <bharathjinka09@gmail.com>
2020-09-15 15:11:55 +0530

    Sveltejs

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  let name = "bharath";
+
+  setTimeout(() => {
+    name = "Ranga";
+  }, 3000);
+
+  let a = 10;
+  let b = 20;
+
+  let myHtml = "<strong>bharath</strong>"
+
+
+</script>
+
+<style>
+</style>
+
+<h1>Hello, {name}</h1>
+
+<h1>{a} + {b} = {a + b}</h1>
+
+<div>some text {@html myHtml}</div>
