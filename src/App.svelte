8160999b5b5fa4b52eb064e3599d1d3db4c8e730
bharathjinka09 <bharathjinka09@gmail.com>
2020-09-16 14:51:29 +0530

    Added img attributes

--- src/App.svelte
+++ src/App.svelte
@@ -8,16 +8,27 @@
   let a = 10;
   let b = 20;
 
-  let myHtml = "<strong>bharath</strong>"
+  let myHtml = "<strong>bharath</strong>";
 
+  let src = "/favicon.png";
+  let channelName = "favicon";
 
+  let disabled = false;
 </script>
 
 <style>
 </style>
 
+<img {src} width="150" alt="{channelName}" />
+
+<input type="text" value="Text" {disabled}>
+
+
 <h1>Hello, {name}</h1>
 
 <h1>{a} + {b} = {a + b}</h1>
 
-<div>some text {@html myHtml}</div>
+<div>
+  some text
+  {@html myHtml}
+</div>
